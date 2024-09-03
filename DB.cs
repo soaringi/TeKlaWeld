0ba9526890f6281eab7a7a20bdff4fb36c334272
11022 <11022@>
2024-09-03 21:22:42 +0800

    添加项目文件。

---
+++ DB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekla.Structures.Model;
+
+namespace TeKlaWeld
+{
+    public class Weld
+    { 
+        public string AssNo {  get; set; }
+        public string profile { get; set; }
+        public string Material { get; set; }
+        public decimal MainLength {  get; set; }
+        public string WeldNo { get; set; }
+        public string ConnectPart { get; set; }
+        public string WeldLength {  get; set; }
+        public PolygonWeld weld { get; set; }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.UI;
+using Tekla.Structures.Geometry3d;
+using ModelObjectSelector = Tekla.Structures.Model.UI.ModelObjectSelector;
+
+
+namespace TeKlaWeld
+{
+    public partial class Form1 : Form
+    {
+        Model Model;
+        public Form1()
+        {
+            InitializeComponent();
+            Model= new Model();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Model==null)
+            {
+                Model = new Model();
+            }
+            ModelObjectSelector se= new ModelObjectSelector  ();
+            var enumm=se.GetSelectedObjects();
+            List<Assembly> assemblies = new List<Assembly>();
+            while (enumm.MoveNext())
+            {
+                var  part= enumm.Current as Part;
+                if (part==null)
+                {
+                    continue;
+                }
+                var ass=part.GetAssembly();
+
+            }
+        }
+    }
+}
